a7f8dcede443df38aa019aa4c0c859d66959fe02
shailae <shailahadli@gmail.com>
2024-03-07 14:19:24 +0530

    demorgan commit

---
+++ demorgan_law.py
@@ -0,0 +1,6 @@
+username=input('enter the valid username: ')
+password=input('enter the valid password: ')
+if username=="shaila" and password=="sha12":
+    print('login successufull')
+else:
+    print('invalid credentials')
